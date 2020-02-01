cf15b9744ee19b8e7702044f2014eba5df514d2e
Kristin Evans <kevans0625@gmail.com>
2020-01-31 19:26:25 -0600

    Removed the div to find siblings to store and display values.

--- assets/script.js
+++ assets/script.js
@@ -12,9 +12,7 @@ $(document).ready(function() {
     var time = moment().format('h:mm a');
     //create variable for current time
     var displayTime =  $(".currenttime").text(time);
-    var eventTime = $("btn-time").val();
-    
-    console.log(eventTime);
+  
 
 
     function currentDate() { 
@@ -27,97 +25,25 @@ $(document).ready(function() {
 
     eventList = JSON.parse(localStorage.getItem('masterList')); 
     // When form is submitted... //works but does grab value
-    $(".btnLock").on("click", function(e) {
+
+    $(".btnLock").on("click", function(e){
         e.preventDefault();
-        //save to storage
-        localStorage.setItem("event1", $(".eventinput").val());
-            // $(".input1").text($(this).text());
-    
-            $(".input2").closest("input").text($(this).text())
-            //pull from storage
-            console.log($("this").closest("input").append(eventList))
-        });
-        
-    $(".btnLock").on("click", function() {
-        //save to storage 
-        localStorage.setItem("event2", $(".input2").val());
-            $(".input2").text($(this).text());
-    });
-    
-    $(".btnLock").on("click", function() {
-        //save to storage
-        localStorage.setItem("event3", $(".input3").val());
-            $(".input3").text($(this).text());
-    });
-    
-    $(".btnLock").on("click", function() {
-        //save to storage
-        localStorage.setItem("event4", $(".input4").val());
-            $(".input4").text($(this).text());
-    });
-    
-    $(".btnLock").on("click", function() {
-        //save to storage
-        localStorage.setItem("event5", $(".input5").val());
-            $(".input5").text($(this).text());
-    });
-    
-    $(".btnLock").on("click", function() {
-        //save to storage
-        localStorage.setItem("event6", $(".input6").val());
-            $(".input6").text($(this).text());
-    });
-    
-    $(".btnLock").on("click", function() {
-        //save to storage
-        localStorage.setItem("event7", $(".input7").val());
-            $(".input7").text($(this).text());
-    });
-    
-    $(".btnLock").on("click", function() {
-        //save to storage
-        localStorage.setItem("event8", $(".input8").val());
-            $(".input8").text($(this).text());
-    });
-    
-    $(".btnLock").on("click", function() {
-        //save to storage
-        localStorage.setItem("event9", $(".input9").val());
-            $(".input9").text($(this).text());
-        });
+        let hourText = $(this).siblings( ".form-control" ).val();
+        let hourTime = $(this).siblings( ".time" ).attr("value");
+
+        localStorage.setItem(hourTime, hourText);
+        console.log(hourText)
+        console.log(hourTime)
+    })
+
+
+ 
         
     //display storage item by getting the itme and appending it
-    function displayIt(){
-        if(localStorage === null){
-            return 
-        }
-        $("#9am").append(localStorage.getItem('event1'))
-        $("#10am").append(localStorage.getItem('event2'))
-        $("#11am").append(localStorage.getItem('event3'))
-        $("#12pm").append(localStorage.getItem('event4'))
-        $("#1pm").append(localStorage.getItem('event5'))
-        $("#2pm").append(localStorage.getItem('event6'))
-        $("#3pm").append(localStorage.getItem('event7'))
-        $("#4pm").append(localStorage.getItem('event8'))
-        $("#5pm").append(localStorage.getItem('event9'))
-     } displayIt()
-    
-    
+   
     //change cell color storage item by getting the itme and appending it
     
-    // function eventTime(){
-    //     if ($("textarea") === ""){
-    //         return 
-    //     } else if (displayTime === "6:35 PM"){
-    //         $("#5pm").closest("textarea").addclass("present");
-    //         console.log("we did it!")
-    //     } else if (displayTime < $("p").value){
-    //         $("p").closest("textarea").addclass("past");
-    
-    //     }
-    // }eventTime()
-    
-    
+
     
     
     
